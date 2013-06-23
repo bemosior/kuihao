bbbb20c07791bcdf6f1e192285a3cc1dd1619f38
Chris McEniry <cmceniry@corgalabs.com>
2013-06-22 18:51:05 -0700

    * simple web server for dealing with xhr needs

---
+++ web.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main() {
+  http.Handle(
+    "/static/",
+    http.StripPrefix(
+      "/static/",
+      http.FileServer(http.Dir("static")),
+    ),
+  )
+  http.ListenAndServe(":9000", nil)
+}
