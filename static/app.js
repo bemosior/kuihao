69823b3001a1499059a08d1a304d719a94939e90
Chris McEniry <cmceniry@corgalabs.com>
2013-08-26 23:37:06 -0700

    * Moved init into own function * Basic init that hard codes some testing shapes

--- static/app.js
+++ static/app.js
@@ -82,58 +82,98 @@ kuihaoApp.controller('MainCtrl', function($scope) {
     return ret;
   };
 
-  var r = Raphael("floor", 640, 480);
+  var floorDiagram;
+  var workCenters, workCenterShapes;
+  var products, productShapes;
+  var floor;
 
   var state = "normal";
-
-  var floor = r.rect( 0, 0, 640, 480);
-
-  var workCenters      = [{
-    "name": "workcenter1",
-    "inputs": [
-      "server",
-      "license",
-    ],
-    "outputs": [
-      "server/licensed",
-    ]
-  }];
-  var workCenterShapes = [];
-  var products         = [{"p1":"1"}];
-  var productShapes    = [];
 
   var workCenterColor  = Raphael.getColor();
   Raphael.getColor(); Raphael.getColor(); Raphael.getColor();
   var productColor     = Raphael.getColor();
 
-  draw_workcenter(workCenters[0]);
-  for (var i=0; i < workCenters.length; i++) {
-    workCenterShapes.push(
-      r.rect(290, 80, 60, 40, 10)
-        .attr({
-          fill: workCenterColor,
-          stroke: workCenterColor,
-          "fill-opacity": 1,
-          "stroke-width": 2,
-          "cursor" : "move",
-        })
-        .drag(drag_move, drag_start)
-        .hover(display_info, display_clear)
-    )
+  var drawProducts = function() {
+    for (var i=0; i < products.length; i++) {
+      productShapes.push(
+        floorDiagram.circle( products[i].loc[0], products[i].loc[1], 10)
+          .attr({
+            fill: productColor,
+            stroke: productColor,
+            "fill-opacity": 1,
+            "stroke-width": 2,
+            "cursor" : "move",
+          })
+          .drag(drag_move, drag_start)
+      );
+    };
   };
 
-  for (var i=0; i < products.length; i++) {
-    productShapes.push(
-      r.circle( 50, 50, 10)
-        .attr({
-          fill: productColor,
-          stroke: productColor,
-          "fill-opacity": 1,
-          "stroke-width": 2,
-          "cursor" : "move",
-        })
-        .drag(drag_move, drag_start)
-    );
+  var drawWorkCenters = function() {
+    for (var i=0; i < workCenters.length; i++) {
+      workCenterShapes.push(
+        floorDiagram.rect(workCenters[i].loc[0], workCenters[i].loc[1], 60, 40, 10)
+          .attr({
+            fill: workCenterColor,
+            stroke: workCenterColor,
+            "fill-opacity": 1,
+            "stroke-width": 2,
+            "cursor" : "move",
+          })
+          .drag(drag_move, drag_start)
+          .hover(display_info, display_clear)
+      );
+    };
+  };
+
+  var drawConnections = function() {
+  };
+
+  $scope.resetInit = function() {
+    if ((typeof floorDiagram === 'undefined') || (floorDiagram === null)) {
+      floorDiagram = Raphael("floor", 640, 480);
+    };
+    floorDiagram.clear();
+    /*
+    workCenters = [];
+    workCenterShapes = [];
+    products = [];
+    productShapes = [];
+    */
+    products = [
+      {
+        id: "7a5041e6-8f92-4bf6-b962-9f75bf9c070d",
+        name: "blade",
+        loc: [100,200],
+      },
+      {
+        id: "eaef65f2-0369-4d05-ace5-123e33486373",
+        name: "oslicense",
+        loc: [200,100],
+      },
+    ]
+    productShapes = [];
+    workCenters = [
+      {
+        id: "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
+        inputs: ["blade", "oslicense"],
+        inputItems: ["7a5041e6-8f92-4bf6-b962-9f75bf9c070d", "eaef65f2-0369-4d05-ace5-123e33486373"],
+        outputs: ["blade w/ oslicense"],
+        loc: [200,200],
+      },
+      {
+        id: "d8e5fa43-63d3-4228-9c93-ff1c64d2371b",
+        inputs: ["blade w/ oslicense"],
+        inputItems: ["4eb234b0-9ea4-45a7-82ec-3f7dd60db20a"],
+        outputs: ["blade w/ os installed"],
+        loc: [300,300],
+      },
+    ]
+    workCenterShapes = [];
+    floor = floorDiagram.rect( 0, 0, 640, 480);
+    drawProducts();
+    drawWorkCenters();
+    drawConnections();
   }
 
   $scope.addWorkCenter = function() {
@@ -141,7 +181,7 @@ kuihaoApp.controller('MainCtrl', function($scope) {
       "wcname": "new wc " + Math.floor(Math.random()*10000 + 1),
     });
     workCenterShapes.push(
-      r.rect(260, 220, 60, 40, 10)
+      floorDiagram.rect(260, 220, 60, 40, 10)
         .attr({
           fill: workCenterColor,
           stroke: workCenterColor,
@@ -159,7 +199,7 @@ kuihaoApp.controller('MainCtrl', function($scope) {
       "pnew": "2"
     })
     productShapes.push(
-      r.circle( 50, 50, 10)
+      floorDiagram.circle( 50, 50, 10)
         .attr({
           fill: productColor,
           stroke: productColor,
