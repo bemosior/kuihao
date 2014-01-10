19c07ecec458bac7cafe4644ebdba48829cd65a8
Chris McEniry <cmceniry@corgalabs.com>
2014-01-09 21:46:17 -0800

    * more complete sample example

--- static/sampledata.js
+++ static/sampledata.js
@@ -1,111 +1,159 @@
 var sampledata = {
   floorinfo : function() {
     return {
-      "222d49c0-2ef7-11e3-aa6e-0800200c9a66": {
-        id: "222d49c0-2ef7-11e3-aa6e-0800200c9a66",
-        name: "Blade Build",
-        flow: [
-          "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
-          "d8e5fa43-63d3-4228-9c93-ff1c64d2371b",
-        ],
+      "b1a0ab07-7108-4de5-b135-c8998e01e020": {
+        id: "b1a0ab07-7108-4de5-b135-c8998e01e020",
+        name: "Host Build",
         stations: {
-          "7a5041e6-8f92-4bf6-b962-9f75bf9c070d": {
-            id: "7a5041e6-8f92-4bf6-b962-9f75bf9c070d",
-            name: "Blade",
-            modified: [],
-            loc: [100,200],
-            type: "product",
-          },
-          "eaef65f2-0369-4d05-ace5-123e33486373": {
-            id: "eaef65f2-0369-4d05-ace5-123e33486373",
-            name: "OS License",
-            modified: [],
-            loc: [200,100],
-            type: "product",
-          },
-          "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a": {
-            id: "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
-            loc: [200,200],
-            type: "workcenter",
-          },
-          "60c90afc-3ea1-405c-b3b9-abc25640e590": {
-            id: "60c90afc-3ea1-405c-b3b9-abc25640e590",
-            name: "Blade",
-            modified: [],
-            loc: [300, 200],
-            type: "product",
-          },
-          "5088cec1-f3d3-4fc8-9014-93cdf3247672": {
-            id: "5088cec1-f3d3-4fc8-9014-93cdf3247672",
-            name: "Centrify License",
-            modified: [],
-            loc: [400, 100],
-            type: "product",
-          },
-          "d8e5fa43-63d3-4228-9c93-ff1c64d2371b": {
-            id: "d8e5fa43-63d3-4228-9c93-ff1c64d2371b",
-            loc: [400,200],
-            type: "workcenter",
-          },
-          "f65e0589-afff-4942-ba2c-4feb72334a85": {
-            id: "f65e0589-afff-4942-ba2c-4feb72334a85",
-            name: "Blade",
-            modified: [],
-            loc: [500,200],
-            type: "product",
-          },
+          "28200dbe-809b-4b56-9ed1-53552453a6b7": {"id":"28200dbe-809b-4b56-9ed1-53552453a6b7","loc":[129,115],"type":"workcenter"},
+          "fd0d68a5-ab39-49fd-a4c6-91cd00b07931": {"id":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931","loc":[337,115],"type":"workcenter"},
+          "d46eecfb-a03d-44a2-b5fc-b5006d7b946e": {"id":"d46eecfb-a03d-44a2-b5fc-b5006d7b946e","loc":[126,196],"type":"workcenter"},
+          "a9872c02-fa66-445a-b0ee-79a15378a5c0": {"id":"a9872c02-fa66-445a-b0ee-79a15378a5c0","loc":[213,275],"type":"workcenter"},
+          "2b4ad91d-4a39-4d33-b0ec-dc759825fe24": {"id":"2b4ad91d-4a39-4d33-b0ec-dc759825fe24","loc":[301,352],"type":"workcenter"},
+          "35330162-dd8f-4733-8cb3-a53e581b1256": {"id":"35330162-dd8f-4733-8cb3-a53e581b1256","loc":[416,267],"type":"workcenter"},
+          "4444c153-5880-47a4-989b-4e1f89d31749": {"id":"4444c153-5880-47a4-989b-4e1f89d31749","loc":[521,353],"type":"workcenter"},
+          "0c96f7d0-cd0d-4be0-b05f-18d9a607cf28": {"id":"0c96f7d0-cd0d-4be0-b05f-18d9a607cf28","loc":[39,121],"type":"product","name":"Server","modified":[]},
+          "44b4ef8b-825c-4b0c-8264-a97b6183f74a": {"id":"44b4ef8b-825c-4b0c-8264-a97b6183f74a","loc":[76,37],"type":"product","name":"Rack Position","modified":[]},
+          "9463dd84-fa98-45d3-b43e-4b2d30e0236a": {"id":"9463dd84-fa98-45d3-b43e-4b2d30e0236a","loc":[373,35],"type":"product","name":"Network Cables","modified":[]},
+          "98124288-e2f7-43ca-ad63-908dabe00f74": {"id":"98124288-e2f7-43ca-ad63-908dabe00f74","loc":[298,35],"type":"product","name":"Power Cables","modified":[]},
+          "c8400f6c-eae0-47f4-9ed2-fd30a1d99e2c": {"id":"c8400f6c-eae0-47f4-9ed2-fd30a1d99e2c","loc":[220,36],"type":"product","name":"Power Receptacle","modified":[]},
+          "140ad88d-d1fe-4f28-94fc-7530119e228d": {"id":"140ad88d-d1fe-4f28-94fc-7530119e228d","loc":[444,38],"type":"product","name":"SwitchPort","modified":[]},
+          "05deb6cb-da28-4d86-b41a-dc3295d01ac6": {"id":"05deb6cb-da28-4d86-b41a-dc3295d01ac6","loc":[298,197],"type":"product","name":"Server","modified":[]},
+          "2dacb945-7509-4156-8cb0-b19753cb7080": {"id":"2dacb945-7509-4156-8cb0-b19753cb7080","loc":[45,197],"type":"product","name":"Server Information","modified":[]},
+          "034a34e3-0167-4b06-aa0e-c3d5e9da93aa": {"id":"034a34e3-0167-4b06-aa0e-c3d5e9da93aa","loc":[215,195],"type":"product","name":"Host Profile","modified":[]},
+          "9b0804f7-cd67-49c6-bac8-255c4192554c": {"id":"9b0804f7-cd67-49c6-bac8-255c4192554c","loc":[213,351],"type":"product","name":"Host","modified":[]},
+          "58b1c8a3-6e92-4769-a03a-be88b9f79115": {"id":"58b1c8a3-6e92-4769-a03a-be88b9f79115","loc":[302,270],"type":"product","name":"csr","modified":[]},
+          "57613fdc-9593-4adb-b7a9-b19a7c00510e": {"id":"57613fdc-9593-4adb-b7a9-b19a7c00510e","loc":[219,111],"type":"product","name":"Server","modified":[]},
+          "781b512e-4152-433f-8807-4e4aff44444e": {"id":"781b512e-4152-433f-8807-4e4aff44444e","loc":[522,267],"type":"product","name":"crt","modified":[]},
+          "e07b50ad-cc2d-482e-9a10-3cef4a3e82e8": {"id":"e07b50ad-cc2d-482e-9a10-3cef4a3e82e8","loc":[414,352],"type":"product","name":"Host","modified":[]},
+          "4603ca45-a84c-49de-85eb-43a83c87217f": {"id":"4603ca45-a84c-49de-85eb-43a83c87217f","loc":[624,352],"type":"product","name":"Host","modified":[]},
         },
         connections: {
-          "7a5041e6-8f92-4bf6-b962-9f75bf9c070d|4eb234b0-9ea4-45a7-82ec-3f7dd60db20a" : {
-            source: "7a5041e6-8f92-4bf6-b962-9f75bf9c070d",
-            destination: "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
-          },
-          "eaef65f2-0369-4d05-ace5-123e33486373|4eb234b0-9ea4-45a7-82ec-3f7dd60db20a" : {
-            source: "eaef65f2-0369-4d05-ace5-123e33486373",
-            destination: "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
-          },
-          "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a|60c90afc-3ea1-405c-b3b9-abc25640e590" : {
-            source: "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
-            destination: "60c90afc-3ea1-405c-b3b9-abc25640e590",
-          },
+          "0c96f7d0-cd0d-4be0-b05f-18d9a607cf28|28200dbe-809b-4b56-9ed1-53552453a6b7":{"source":"0c96f7d0-cd0d-4be0-b05f-18d9a607cf28","destination":"28200dbe-809b-4b56-9ed1-53552453a6b7"},
+          "44b4ef8b-825c-4b0c-8264-a97b6183f74a|28200dbe-809b-4b56-9ed1-53552453a6b7":{"source":"44b4ef8b-825c-4b0c-8264-a97b6183f74a","destination":"28200dbe-809b-4b56-9ed1-53552453a6b7"},
+          "28200dbe-809b-4b56-9ed1-53552453a6b7|57613fdc-9593-4adb-b7a9-b19a7c00510e":{"source":"28200dbe-809b-4b56-9ed1-53552453a6b7","destination":"57613fdc-9593-4adb-b7a9-b19a7c00510e"},
+          "57613fdc-9593-4adb-b7a9-b19a7c00510e|fd0d68a5-ab39-49fd-a4c6-91cd00b07931":{"source":"57613fdc-9593-4adb-b7a9-b19a7c00510e","destination":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931"},
+          "c8400f6c-eae0-47f4-9ed2-fd30a1d99e2c|fd0d68a5-ab39-49fd-a4c6-91cd00b07931":{"source":"c8400f6c-eae0-47f4-9ed2-fd30a1d99e2c","destination":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931"},
+          "98124288-e2f7-43ca-ad63-908dabe00f74|fd0d68a5-ab39-49fd-a4c6-91cd00b07931":{"source":"98124288-e2f7-43ca-ad63-908dabe00f74","destination":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931"},
+          "9463dd84-fa98-45d3-b43e-4b2d30e0236a|fd0d68a5-ab39-49fd-a4c6-91cd00b07931":{"source":"9463dd84-fa98-45d3-b43e-4b2d30e0236a","destination":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931"},
+          "140ad88d-d1fe-4f28-94fc-7530119e228d|fd0d68a5-ab39-49fd-a4c6-91cd00b07931":{"source":"140ad88d-d1fe-4f28-94fc-7530119e228d","destination":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931"},
+          "2dacb945-7509-4156-8cb0-b19753cb7080|d46eecfb-a03d-44a2-b5fc-b5006d7b946e":{"source":"2dacb945-7509-4156-8cb0-b19753cb7080","destination":"d46eecfb-a03d-44a2-b5fc-b5006d7b946e"},
+          "d46eecfb-a03d-44a2-b5fc-b5006d7b946e|034a34e3-0167-4b06-aa0e-c3d5e9da93aa":{"source":"d46eecfb-a03d-44a2-b5fc-b5006d7b946e","destination":"034a34e3-0167-4b06-aa0e-c3d5e9da93aa"},
+          "034a34e3-0167-4b06-aa0e-c3d5e9da93aa|a9872c02-fa66-445a-b0ee-79a15378a5c0":{"source":"034a34e3-0167-4b06-aa0e-c3d5e9da93aa","destination":"a9872c02-fa66-445a-b0ee-79a15378a5c0"},
+          "fd0d68a5-ab39-49fd-a4c6-91cd00b07931|05deb6cb-da28-4d86-b41a-dc3295d01ac6":{"source":"fd0d68a5-ab39-49fd-a4c6-91cd00b07931","destination":"05deb6cb-da28-4d86-b41a-dc3295d01ac6"},
+          "05deb6cb-da28-4d86-b41a-dc3295d01ac6|a9872c02-fa66-445a-b0ee-79a15378a5c0":{"source":"05deb6cb-da28-4d86-b41a-dc3295d01ac6","destination":"a9872c02-fa66-445a-b0ee-79a15378a5c0"},
+          "a9872c02-fa66-445a-b0ee-79a15378a5c0|9b0804f7-cd67-49c6-bac8-255c4192554c":{"source":"a9872c02-fa66-445a-b0ee-79a15378a5c0","destination":"9b0804f7-cd67-49c6-bac8-255c4192554c"},
+          "9b0804f7-cd67-49c6-bac8-255c4192554c|2b4ad91d-4a39-4d33-b0ec-dc759825fe24":{"source":"9b0804f7-cd67-49c6-bac8-255c4192554c","destination":"2b4ad91d-4a39-4d33-b0ec-dc759825fe24"},
+          "2b4ad91d-4a39-4d33-b0ec-dc759825fe24|58b1c8a3-6e92-4769-a03a-be88b9f79115":{"source":"2b4ad91d-4a39-4d33-b0ec-dc759825fe24","destination":"58b1c8a3-6e92-4769-a03a-be88b9f79115"},
+          "2b4ad91d-4a39-4d33-b0ec-dc759825fe24|e07b50ad-cc2d-482e-9a10-3cef4a3e82e8":{"source":"2b4ad91d-4a39-4d33-b0ec-dc759825fe24","destination":"e07b50ad-cc2d-482e-9a10-3cef4a3e82e8"},
+          "58b1c8a3-6e92-4769-a03a-be88b9f79115|35330162-dd8f-4733-8cb3-a53e581b1256":{"source":"58b1c8a3-6e92-4769-a03a-be88b9f79115","destination":"35330162-dd8f-4733-8cb3-a53e581b1256"},
+          "35330162-dd8f-4733-8cb3-a53e581b1256|781b512e-4152-433f-8807-4e4aff44444e":{"source":"35330162-dd8f-4733-8cb3-a53e581b1256","destination":"781b512e-4152-433f-8807-4e4aff44444e"},
+          "781b512e-4152-433f-8807-4e4aff44444e|4444c153-5880-47a4-989b-4e1f89d31749":{"source":"781b512e-4152-433f-8807-4e4aff44444e","destination":"4444c153-5880-47a4-989b-4e1f89d31749"},
+          "4444c153-5880-47a4-989b-4e1f89d31749|4603ca45-a84c-49de-85eb-43a83c87217f":{"source":"4444c153-5880-47a4-989b-4e1f89d31749","destination":"4603ca45-a84c-49de-85eb-43a83c87217f"},
+          "e07b50ad-cc2d-482e-9a10-3cef4a3e82e8|4444c153-5880-47a4-989b-4e1f89d31749":{"source":"e07b50ad-cc2d-482e-9a10-3cef4a3e82e8","destination":"4444c153-5880-47a4-989b-4e1f89d31749"},
         },
+        flow: [
+          "28200dbe-809b-4b56-9ed1-53552453a6b7",
+          "fd0d68a5-ab39-49fd-a4c6-91cd00b07931",
+          "d46eecfb-a03d-44a2-b5fc-b5006d7b946e",
+          "a9872c02-fa66-445a-b0ee-79a15378a5c0",
+          "2b4ad91d-4a39-4d33-b0ec-dc759825fe24",
+          "35330162-dd8f-4733-8cb3-a53e581b1256",
+          "4444c153-5880-47a4-989b-4e1f89d31749",
+        ],
       },
     };
   },
   workcenterinfo : function() {
     return {
-      "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a": {
-        id: "4eb234b0-9ea4-45a7-82ec-3f7dd60db20a",
-        name: "KVM/Remote Console",
+      "28200dbe-809b-4b56-9ed1-53552453a6b7" : {
+        id: "28200dbe-809b-4b56-9ed1-53552453a6b7",
+        name: "Data Center (Racking)",
         products: [
-          {
-            name: 'Blade',
-            type: 'modified',
-            change: '+os',
-          },
-          {
-            name: 'OS License',
-            type: 'input',
-          },
+          {"name":"Server","type":"modified","change":"racked"},
+          {"name":"Rack Position","type":"input"},
         ],
-        steps: [],
+        steps: [
+          {"text":"Rack the server into the proper location.","full":""},
+        ],
       },
-      "d8e5fa43-63d3-4228-9c93-ff1c64d2371b": {
-        id: "d8e5fa43-63d3-4228-9c93-ff1c64d2371b",
-        name: "adjoin",
+      "fd0d68a5-ab39-49fd-a4c6-91cd00b07931": {
+        id: "fd0d68a5-ab39-49fd-a4c6-91cd00b07931",
+        name: "Data Center (Cabling)",
         products: [
-          {
-            name: 'Blade',
-            type: 'modified',
-            change: '+adclient',
-          },
-          {
-            name: 'Centrify License',
-            type: 'input',
-          },
+          {"name":"Server","type":"modified","change":"cabled"},
+          {"name":"SwitchPort","type":"input"},
+          {"name":"Network Cables","type":"input"},
+          {"name":"Power Receptacle","type":"input"},
+          {"name":"Power Cables","type":"input"},
         ],
-        steps: [],
+        steps: [
+          {"text":"Plug in primary power supply into power receptacle.","full":"Plug in redundant power supplies."},
+          {"text":"Plug in secondary power supply into power receptacle on separate circuit.","full":""},
+          {"text":"Plug the network cables into the right switchports.","full":""},
+        ],
       },
-    };
+      "d46eecfb-a03d-44a2-b5fc-b5006d7b946e": {
+        id: "d46eecfb-a03d-44a2-b5fc-b5006d7b946e",
+        name: "Cobbler",
+        products: [
+          {"name":"Server Information","type":"input"},
+          {"name":"Host Profile","type":"output"},
+        ],
+        steps: [
+          {"text":"Log into cobbler.","full":""},
+          {"text":"Configure host profile.","full":""},
+        ],
+      },
+      "a9872c02-fa66-445a-b0ee-79a15378a5c0": {
+        id: "a9872c02-fa66-445a-b0ee-79a15378a5c0",
+        name: "Server Console",
+        products: [
+          {"name":"Server","type":"input"},
+          {"name":"Host Profile","type":"input"},
+          {"name":"Host","type":"output"},
+        ],
+        steps: [
+          {"text":"Power on Server.","full":""},
+          {"text":"Enter BIOS and select network boot.","full":""},
+        ],
+      },
+      "4444c153-5880-47a4-989b-4e1f89d31749": {
+        id: "4444c153-5880-47a4-989b-4e1f89d31749",
+        name: "Host (firstrun puppet)",
+        products: [
+          {"name":"Host","type":"modified","change":"configured"},
+          {"name":"crt","type":"input"}
+        ],
+        steps: [
+          {"text":"Log into host via SSH.","full":""},
+          {"text":"Run puppet first the first configuration run.","full":""},
+        ],
+      },
+      "35330162-dd8f-4733-8cb3-a53e581b1256": {
+        id: "35330162-dd8f-4733-8cb3-a53e581b1256",
+        name: "PuppetMaster (csr signing)",
+        products: [
+          {"name":"csr","type":"input"},
+          {"name":"crt","type":"output"},
+        ],
+        steps: [
+          {"text":"Log onto the Puppet Master.","full":""},
+          {"text":"Run puppetca to sign certificate.","full":""},
+        ],
+      },
+      "2b4ad91d-4a39-4d33-b0ec-dc759825fe24": {
+        id: "2b4ad91d-4a39-4d33-b0ec-dc759825fe24",
+        name: "Host (firstrun ssh)",
+        products: [
+          {"name":"Host","type":"modified","change":""},
+          {"name":"csr","type":"output"},
+        ],
+        steps: [
+          {"text":"Log into host via ssh.","full":""},
+          {"text":"Run puppet once","full":""},
+        ],
+      },
+    }
   },
 };
